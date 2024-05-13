1a9b0206f46474d06942700f532594e3d2ce7926
Amit <74492526+amitrizz@users.noreply.github.com>
2024-05-13 14:49:34 +0530

    Update entrypoint.sh

--- myproject/scripts/entrypoint.sh
+++ myproject/scripts/entrypoint.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
 
+echo "Collecting static files..."
+python manage.py collectstatic --noinput || { echo "Failed to collect static files"; exit 1; }
+
+echo "Making migrations..."
+python manage.py makemigrations
+python manage.py makemigrations posts users || { echo "Failed to make migrations"; exit 1; }
+
+
+echo "Applying migrations..."
+python manage.py migrate
+echo "Applying migrations for posts app..."
+python manage.py migrate posts || { echo "Failed to apply migrations for accounts app"; exit 1; }
+echo "Applying migrations for users app..."
+python manage.py migrate users || { echo "Failed to apply migrations for booking app"; exit 1; }
+
+echo "All migrations applied successfully."
+
+echo "Creating superuser..."
+# python manage.py createsuperuser --noinput --username=admin --email=admin@gmail.com --password=admin || { echo "Failed to create superuser"; exit 1; }
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@gmail.com', 'admin')" | python manage.py shell || { echo "Failed to create superuser"; exit 1; }
+
+exec "$@"
